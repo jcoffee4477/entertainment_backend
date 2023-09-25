54e76052a3263478f55b3a7ae36f3dd9f2869326
jcoffee4477 <jacobcoffee@jacobs-air.mynetworksettings.com>
2023-09-25 12:01:50 -0400

    update action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,4 +20,15 @@ def create
   render :show
 end
 
+def update
+  @item = Item.find_by(id: params[:id])
+  @item.update(
+    name: params[:name] || @item.name,
+    image_url: params[:image_url] || @item.image_url,
+    description: params[:description] || @item.description,
+    category: params[:category] || @item.category
+  )
+  render :show 
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get "/items" => "items#index"
   get "/items/:id" => "items#show"
   post "/items" => "items#create"
+  patch "/items/:id" => "items#update"
 end
